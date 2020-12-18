1b731460dfaf1791ee1ca61b4103a41796d97fea
zhukai <zhukai@foresee.com.cn>
2020-12-18 10:35:57 +0800

    第一个flutter_app

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,19 @@
 import 'package:flutter/material.dart';
-import 'apps/apps.dart';
-import 'adjust/adjust.dart';
-import 'person/person.dart';
-import 'shopping/shopping.dart';
-import 'homePage/homePage.dart';
 
-void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget {
+
+import 'routes/Routes.dart';
+
+void main() => runApp(MyApp());
+class MyApp extends StatelessWidget {  
+  
   @override
   Widget build(BuildContext context) {
-    // final wordPair = new WordPair.random();
-    return new MaterialApp(
-      title: 'flutter app',
-      theme: new ThemeData(
-        primaryColor: Colors.red,
-      ),
-      home: new RandomWords(),
-    );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  createState() => new RandomWordsState();
-}
-
-class RandomWordsState extends State<RandomWords> {
-  final pages = [HomePage(), Apps(), Adjust(), Shopping(), Person()];
-  int _currentIndex = 0;
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        items: [
-          BottomNavigationBarItem(
-            title: Text(
-              "首页",
-              // style: TextStyle(color: Colors.red),
-            ),
-            icon: Icon(Icons.home),
-          ),
-          BottomNavigationBarItem(
-            title: Text(
-              "分类",
-              // style: TextStyle(color: Colors.red),
-            ),
-            icon: Icon(Icons.apps),
-          ),
-          BottomNavigationBarItem(
-            title: Text(
-              "发现",
-              // style: TextStyle(color: Colors.red),
-            ),
-            icon: Icon(Icons.adjust),
-          ),
-          BottomNavigationBarItem(
-            title: Text(
-              "购物车",
-              // style: TextStyle(color: Colors.red),
-            ),
-            icon: Icon(Icons.shopping_cart),
-          ),
-          BottomNavigationBarItem(
-            title: Text(
-              "我的",
-              // style: TextStyle(color: Colors.red),
-            ),
-            icon: Icon(Icons.person),
-          ),
-        ],
-        currentIndex: _currentIndex,
-        showUnselectedLabels: true,
-        onTap: (context) {
-          setState(() {
-            _currentIndex = context;
-          });
-        },
-        selectedItemColor: Colors.blue,
-      ),
-      body: pages[_currentIndex],
+    return MaterialApp(
+      debugShowCheckedModeBanner:false ,  //去掉debug图标
+      // home:Tabs(),   
+      initialRoute: '/',     //初始化的时候加载的路由
+      onGenerateRoute: onGenerateRoute,
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hellow_world/main.dart';
+import 'package:flutter_app01/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
